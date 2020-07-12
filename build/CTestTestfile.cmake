488445cf1425855a724c56abbcfc5ce5cc03386d
yashsehgal <yashsehgal.work@gmail.com>
2020-07-12 19:51:57 +0530

    deleted cmake files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yashs/Desktop/user-verification-process-website
-# Build directory: C:/Users/yashs/Desktop/user-verification-process-website/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
