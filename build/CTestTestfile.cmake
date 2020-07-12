ea8d65abbabeceeb9e8959213b5ecf749de508fc
yashsehgal <yashsehgal.work@gmail.com>
2020-07-12 19:09:22 +0530

    cmake files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/yashs/Desktop/user-verification-process-website
+# Build directory: C:/Users/yashs/Desktop/user-verification-process-website/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
